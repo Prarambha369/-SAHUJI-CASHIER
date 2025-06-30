158d9134b88e19fc16d329ed26044523285110d6
mrbashyal <prarambhabashyal@gmail.com>
2025-06-30 10:47:33 +0545

    refactor(sahuji.cpp): standardize header includes and improve screen clearing method

--- sahuji.cpp
+++ sahuji.cpp
@@ -1,14 +1,14 @@
-#include >iostream>
-#include >iomanip>
-#include >string>
-#include >cstdlib>
-#include >cstring>
-#include >stdio.h>
-#include >conio.h>
-#include >fstream>
-#include >windows.h>
-#include >cstdio>
-#include >time.h>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+#include <cstring>
+#include <stdio.h>
+#include <fstream>
+#include <cstdio>
+#include <time.h>
+#include <termios.h>
+#include <unistd.h>
 using namespace std;
 
 int k=7,r=0,flag=0;
@@ -19,72 +19,44 @@ char name1[50],ctn1[50];
 int age1,cradit1;
 float points1=0,mbuy1=0;
 
-
 void craditcard();
 void admin();
 void user();
 void design(int a,char ch)// function is used to design at the top of the screen
 {
 	cout<<"\n\n\n";
-	system("cls");
+	std::cout << "\033[2J\033[1;1H";  // Clears screen using ANSI codes, works on most terminals
 	cout<<"\t\t\t"<<setw(a+5)<<setfill(ch);
 	cout<<"\n\n\t\t\t\t\t"<<"SAHUJI";
 	cout<<"\n\n\t\t\t"<<setw(a)<<setfill(ch);
 	cout<<"\n\n";
 	}
 
-COORD coord = {0, 0};
-void gotoxy(int x, int y) // function is used to set the coordinate in output screen
-{
-    COORD coord;
-    coord.X = x;
-    coord.Y = y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+void gotoxy(int x, int y) {
+    std::cout << "\033[" << y << ";" << x << "H";
 }
 
-	struct date
+struct date
 {
     int mm,dd,yy;
 };
 
-string getpass(const char *prompt, bool show_asterisk = true)
-{
-  const char BACKSPACE=8;
-  const char RETURN=13;
-
-  string password;
-  unsigned char ch=0;
-
-  cout <<prompt<<endl;
-
-  DWORD con_mode;
-  DWORD dwRead;
-
-  HANDLE hIn=GetStdHandle(STD_INPUT_HANDLE);
-
-  GetConsoleMode( hIn, &con_mode );
-  SetConsoleMode( hIn, con_mode & ~(ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT) );
-
-  while(ReadConsoleA( hIn, &ch, 1, &dwRead, NULL) && ch !=RETURN)
-    {
-       if(ch==BACKSPACE)
-         {
-            if(password.length()!=0)
-              {
-                 if(show_asterisk)
-                     cout <<"\b \b";
-                 password.resize(password.length()-1);
-              }
-         }
-       else
-         {
-             password+=ch;
-             if(show_asterisk)
-                 cout <<'*';
-         }
+string getpass(const char *prompt, bool show_asterisk = true) {
+    termios oldt, newt;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~ECHO;  // Disable echo
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    string password;
+    char ch;
+    cout << prompt;
+    while ((ch = getchar()) != '\n') {
+        if (show_asterisk) std::cout << '*';
+        password += ch;
     }
-  cout <<endl;
-  return password;
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);  // Restore terminal settings
+    std::cout << std::endl;
+    return password;
 }
 
 class project // base and main class where almost every thing is done.
@@ -380,7 +352,7 @@ void amount::report()
         cout<<"PRESS ANY KEY TO CONTINUE...";
         getch();
         k=7;
-        system("cls");
+        system("clear");
         gotoxy(30,3);
         cout<<" ITEM DETAILS ";
         gotoxy(3,5);
@@ -986,7 +958,7 @@ while(ch=='y'||ch=='Y')
 	em.close();
 	 //em.seekg(0);
 		 //	em.read(reinterpret_cast<char*>(&co),sizeof(co));
-		 system("cls");
+		 system("clear");
             cout<<"\n\t\t============================================================";
 		 	cout<<"\n\t\t\t\t     The Customer ID detail :";
             cout<<"\n\t\t============================================================\n";
@@ -1077,7 +1049,7 @@ disp();
         cout<<"PRESS ANY KEY TO CONTINUE...";
         getch();
         k=7;
-        system("cls");
+        system("clear");
         gotoxy(30,3);
         cout<<" ITEM DETAILS ";
         gotoxy(3,5);
